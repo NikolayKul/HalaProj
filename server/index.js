c3ded3118cfb38c66d049a6a8169f5f703641644
Nikolay <gN1kolaykul@gmail.com>
2017-05-14 00:12:59 +0300

    Add HOST as an env

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ import logger from './utils/logger'
 
 dotenv.config()
 
-const app = express(),
-  PORT = process.env.PORT || 3000
-
+const app = express()
 app.get('/', (req, res) => res.send('Hello world!'))
-
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send("Something went wrong")
 })
 
-app.listen(PORT, () => logger(`Server is running on port ${PORT}`))
+const HOST = process.env.HOST || "localhost",
+  PORT = process.env.PORT || 3000
+app.listen(PORT, HOST, () => logger(`Server is running on ${HOST}:${PORT}`))
